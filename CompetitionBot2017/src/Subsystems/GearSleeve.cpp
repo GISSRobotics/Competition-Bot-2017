48408382d25a309be599c95aaca41e777888ff43
Kegoir <firegoober@gmail.com>
2017-03-08 18:06:50 -0800

    Update GearSleeve.cpp

--- CompetitionBot2017/src/Subsystems/GearSleeve.cpp
+++ CompetitionBot2017/src/Subsystems/GearSleeve.cpp
@@ -8,13 +8,13 @@ void GearSleeve::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
 	// SetDefaultCommand(new MySpecialCommand());
 }
-
+//Toggle current gear sleeve state and update Dashboard
 void GearSleeve::Toggle() {
 	isUp = !isUp;
 	isUpStatus = isUp;
 	frc::SmartDashboard::PutBoolean("Gear Up?", isUpStatus);
 }
-
+//Raise gear sleeve and update Dashboard
 void GearSleeve::Raise() {
 	isUp = true;
 	isUpStatus = true;
@@ -22,6 +22,7 @@ void GearSleeve::Raise() {
 }
 
 bool GearSleeve::Update() {
+	//If not defined, define variables
 	if (!definedYet) {
 		loadedSwitch = new frc::DigitalInput(GEAR_STATUS_SWITCH);
 		gearMotor = new frc::VictorSP(GEAR_MOTOR);
@@ -29,11 +30,13 @@ bool GearSleeve::Update() {
 		downLimit = new frc::DigitalInput(GEAR_LIMIT_SWITCH_DOWN);
 		definedYet = true;
 	}
+	//Updates variables and dashboard
 	isLoaded = !loadedSwitch->Get();
 	frc::SmartDashboard::PutBoolean("Gear Loaded?", isLoaded);
 	if (isUp) {
 		isUpStatus = !isUpStatus;
 	}
+	//Update gear motor, and if too far, shutdown
 	if (isUp&&upLimit->Get()<1) {
 		gearMotor->Set(upPower);
 	} else if (!isUp&&downLimit->Get()) {
@@ -42,12 +45,13 @@ bool GearSleeve::Update() {
 	} else {
 		gearMotor->Set(0.0);
 	}
+	//Update dashboard
 	frc::SmartDashboard::PutBoolean("Gear Up?", isUpStatus);
 	frc::SmartDashboard::PutBoolean("Upper Limit", upLimit->Get());
 	frc::SmartDashboard::PutBoolean("Lower Limit", downLimit->Get());
 	return isLoaded;
 }
-
+//Reset bools and dashboard
 void GearSleeve::Reset() {
 	isUp = false;
 	isUpStatus = false;
